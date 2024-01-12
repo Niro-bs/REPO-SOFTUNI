9954fa4ed9cff30f7f7b1813bc73ec227556fdfb
Aleko Neshev <alekonesh@gmail.com>
2024-01-12 12:28:18 +0200

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+	
+	document.getElementById("btnAdd").addEventListener("click", addTown);
+	});
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
@@ -16,4 +21,23 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+function addTown() {
+	let townName = document.getElementById("townNameForAdd").value;
+	if (townName != "") {
+		document.getElementById("townNameForAdd").value = "";
+let option = document.createElement("option");
+option.textContent = townName;
+document.getElementById("towns").appendChild(option);
+showMessage(townName + " added.");
+}
+}
+
+function showMessage(msg) {
+let resultElement = document.getElementById("result");
+resultElement.textContent = msg;
+resultElement.style.display = "block";
+setTimeout(function () {
+resultElement.style.display = "none";
+}, 3000);
 }
