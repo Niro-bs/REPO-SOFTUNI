4e87f603d62dd3375a6a3c24d10218c84516a428
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2024-01-12 12:30:44 +0200

    Shuffle

--- towns.js
+++ towns.js
@@ -17,3 +17,37 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+function shuffleTowns() {
+	let towns = Array.from(document.querySelectorAll("#towns option"));
+	document.getElementById("towns").innerHTML = "";
+	shuffleArray(towns);
+	towns.forEach(function (town) {
+		document.getElementById("towns").appendChild(town);
+	});
+	showMessage("Towns shuffled");
+
+	function shuffleArray(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			let j = Math.floor(Math.random() * (i + 1));
+			let oldElement = array[i];
+			array[i] = array[j];
+			array[j] = oldElement;
+		}
+	}
+}
+
+function showMessage(msg) {
+	let resultElement = document.getElementById("result");
+	resultElement.textContent = msg;
+	resultElement.style.display = "block";
+	setTimeout(function () {
+		resultElement.style.display = "none";
+	}, 3000);
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	document.getElementById("btnShuffle").addEventListener("click", shuffleTowns);
+});
