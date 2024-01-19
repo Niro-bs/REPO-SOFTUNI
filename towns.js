12b8eaacc01d96d92eacd2b1c057dfff6f146086
Miroslav Mirtchev <80381657+miroslavmirtchev@users.noreply.github.com>
2024-01-19 11:07:31 +0200

    Styling Changes

--- towns.js
+++ towns.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 });
 
 function deleteTown() {
+<<<<<<< Updated upstream
 let townName = document.getElementById(&quot;townName&quot;).value;
 document.getElementById(&quot;townName&quot;).value = &quot;&quot;;
 let removed = false;
@@ -27,8 +28,34 @@ setTimeout(function () {
 resultElement.style.display = &quot;none&quot;;
 }, 3000);
 }
+=======
+	let townName = document.getElementById("townName").value;
+	document.getElementById("townName").value = "";
+	let removed = false;
+	let options = document.querySelectorAll("#towns option");
+	for (let i = 0; i &lt; options.length; i++) {
+		if (options[i].textContent == townName) {
+			removed = true;
+			options[i].remove();
+		}
+	}
+>>>>>>> Stashed changes
 	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+		showMessage(townName + "deleted");
+else
+	showMessage(townName + "not found.");
+}
+
+function showMessage(msg) {
+	let resultElement = document.getElementById("result");
+	resultElement.textContent = msg;
+	resultElement.style.display = "block";
+	setTimeout(function () {
+		resultElement.style.display = "none";
+	}, 3000);
+}
+if (removed)
+	$('#result').text(townName + " deleted.");
+else
+	$('#result').text(townName + " not found.");
 }
