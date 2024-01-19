15400d4463002d343288a501defd3f6a3df5a5fc
Miroslav Mirtchev <80381657+miroslavmirtchev@users.noreply.github.com>
2024-01-19 10:44:30 +0200

    Update towns.js
    Styling

--- towns.js
+++ towns.js
@@ -3,15 +3,30 @@ $(document).ready(function() {
 });
 
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
+let townName = document.getElementById(&quot;townName&quot;).value;
+document.getElementById(&quot;townName&quot;).value = &quot;&quot;;
+let removed = false;
+let options = document.querySelectorAll(&quot;#towns option&quot;);
+for (let i = 0; i &lt; options.length; i++) {
+if (options[i].textContent == townName) {
+removed = true;
+options[i].remove();
+   }
+}
+if (removed)
+showMessage(townName + &quot; deleted.&quot;);
+else
+showMessage(townName + &quot; not found.&quot;);
+}
+
+function showMessage(msg) {
+let resultElement = document.getElementById(&quot;result&quot;);
+resultElement.textContent = msg;
+resultElement.style.display = &quot;block&quot;;
+setTimeout(function () {
+resultElement.style.display = &quot;none&quot;;
+}, 3000);
+}
 	if (removed)
 		$('#result').text(townName + " deleted.");
 	else
